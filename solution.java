86133d22e6921565eaf3b5748788f23195a45c14
Kooreis <kooreis.official@gmail.com>
2024-04-17 02:02:25 -0500

    In the second update, two integer arrays 'pushed' and 'popped' are created to store the sequences of pushed and popped operations. The user is prompted to enter these sequences, which are then stored in the respective arrays.

--- solution.java
+++ solution.java
@@ -1,10 +1,10 @@
-import java.util.Stack;
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number of operations:");
-        int n = scanner.nextInt();
-    }
+int[] pushed = new int[n];
+int[] popped = new int[n];
+System.out.println("Enter the pushed sequence:");
+for (int i = 0; i < n; i++) {
+    pushed[i] = scanner.nextInt();
+}
+System.out.println("Enter the popped sequence:");
+for (int i = 0; i < n; i++) {
+    popped[i] = scanner.nextInt();
 }
